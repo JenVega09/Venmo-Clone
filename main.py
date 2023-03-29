0bdd9fec1895a32567e820c0d95f8693e9487af5
JenVega09 <JenVega09@gmail.com>
2023-03-29 11:11:06 -0500

    initial commit of bonus_stories branch

--- main.py
+++ main.py
@@ -44,42 +44,42 @@ while inputting_user_name == True:
 
 # Displaying Account Information
 
-print(" ")
-print("Displaying current account information...")
-print(f"Your current available balance is: ${user_one['account_balance']}")
-print(" ")
-print("Your available funds from connected banks are as follows:")
-for funds in user_one['connected_banks']:
-    print(f"{funds[0]}: ${funds[1]}")
-print(" ")
+# print(" ")
+# print("Displaying current account information...")
+# print(f"Your current available balance is: ${user_one['account_balance']}")
+# print(" ")
+# print("Your available funds from connected banks are as follows:")
+# for funds in user_one['connected_banks']:
+#     print(f"{funds[0]}: ${funds[1]}")
+# print(" ")
 
-# Beginning transaction
+# # Beginning transaction
 
-transfer_funds = input(f"Are you sure that you want to transfer money to {user_two['full_name']}? (y/n): ").lower()
-transfer_confirmed = True
-while transfer_confirmed == True:
+# transfer_funds = input(f"Are you sure that you want to transfer money to {user_two['full_name']}? (y/n): ").lower()
+# transfer_confirmed = True
+# while transfer_confirmed == True:
     
-    if transfer_funds == 'n':
-        print("Transaction cancelled.  Have a nice day!")
-        transfer_confirmed = False
-    else:
-        funds_available = False
-        transfer_amount = int(input(f"\nHow much money would you like to transfer to {user_two['full_name']}? (Whole dollars only): "))
-        while funds_available == False:
-            if transfer_amount > user_one['account_balance']:
-                print(f"Insufficient Funds.  Your transfer must be less than or equal to {user_one['account_balance']}.  Please enter a smaller amount to transfer.")
-                transfer_amount = int(input(f"How much money would you like to transfer to {user_two['account_balance']}? (Whole dollars only): "))
-            else: 
-                funds_available = True
+#     if transfer_funds == 'n':
+#         print("Transaction cancelled.  Have a nice day!")
+#         transfer_confirmed = False
+#     else:
+#         funds_available = False
+#         transfer_amount = int(input(f"\nHow much money would you like to transfer to {user_two['full_name']}? (Whole dollars only): "))
+#         while funds_available == False:
+#             if transfer_amount > user_one['account_balance']:
+#                 print(f"Insufficient Funds.  Your transfer must be less than or equal to {user_one['account_balance']}.  Please enter a smaller amount to transfer.")
+#                 transfer_amount = int(input(f"How much money would you like to transfer to {user_two['account_balance']}? (Whole dollars only): "))
+#             else: 
+#                 funds_available = True
 
-        print(f"\nInitiating transfer of {transfer_amount} to \"{user_two['full_name']}\".\n")
-        user_one['account_balance']-=transfer_amount
-        user_two['account_balance']+=transfer_amount
-        print(f"Transaction successful!  You now have ${user_one['account_balance']} remaining.")
+#         print(f"\nInitiating transfer of {transfer_amount} to \"{user_two['full_name']}\".\n")
+#         user_one['account_balance']-=transfer_amount
+#         user_two['account_balance']+=transfer_amount
+#         print(f"Transaction successful!  You now have ${user_one['account_balance']} remaining.")
         
-        additional_transaction=input("Would you like to make an additional transfer? (y/n): ").lower()
-        if additional_transaction == 'n':
-            print(f"\nYou now have {user_one['account_balance']} left in your account.  Have a GREAT day!")
-            transfer_confirmed = False
+#         additional_transaction=input("Would you like to make an additional transfer? (y/n): ").lower()
+#         if additional_transaction == 'n':
+#             print(f"\nYou now have {user_one['account_balance']} left in your account.  Have a GREAT day!")
+#             transfer_confirmed = False
       
 
