00bf11cfeb32d5246d7c0bb9f6d4ef82292de4d2
JenVega09 <JenVega09@gmail.com>
2023-04-01 19:02:58 -0500

    Added single purpose functions

--- main.py
+++ main.py
@@ -61,44 +61,88 @@ def password_check (payor):
 
 def display_account_info(payor):
 
-    print(" ")
-    print("Displaying current account information...")
+    print("\nDisplaying current account information...")
     print(f"Your current available balance is: ${payor['account_balance']}\n")
     print("Your available funds from connected banks are as follows:")
     for funds in payor['connected_banks']:
         print(f"{funds[0]}: ${funds[1]}")
 
+def confirming_transaction (recipient):
+    user_input = input(f"\nAre you sure that you want to transfer mney to {recipient['full_name']}? y/n: ").lower()
+    if user_input == 'y':
+        return True
+    else:
+        return False
+
+def get_transfer_amount (recipient):
+    money_to_transfer = int(input(f"\nHow much money would you like to transfer to {recipient['full_name']}? (Whole dollars only): "))
+    return money_to_transfer
+
+def check_fund_availability (money_to_transfer, payor, recipient):
+    if money_to_transfer > payor['account_balance']:
+        return False
+    else: 
+        return True
+
+def perform_transfer (money_to_transfer, payor, recipient):
+    payor['account_balance'] -= money_to_transfer
+    recipient['account_balance'] += money_to_transfer
+    print (f"\n Transaction successful!!  You now have {payor['account_balance']} left in your account")
+
+def additional_transfer_check (recipient):
+    user_input = input(f"\n Would you like to make another transfer to {recipient['full_name']}? y/n: ").lower()
+    if user_input == 'y':
+        return True
+    else:
+        return False
+
+def end_message (payor):
+    print (f"End of transaction.  You now have ${payor['account_balance']} remaining in your account.  Have a GREAT day!")
+
+user_name_check (payor)
+password_check (payor)
+display_account_info (payor)
+
+performing_transactions = confirming_transaction (recipient)
+while performing_transactions == True:
+        money_to_transfer = get_transfer_amount (recipient)
+        funds_available = check_fund_availability (money_to_transfer, payor, recipient)
+        if funds_available == True:
+            perform_transfer (money_to_transfer,payor, recipient)    
+            performing_transactions = additional_transfer_check (recipient)
+        else:
+            print (f"Insufficient Funds.  You must enter amount less than or equal to {payor['account_balance']}.  ")
+else:
+    end_message (payor)
+
+
 # Transaction Function
 
-def transaction(payor,recipient):
-    transfer_funds = input(f"Are you sure that you want to transfer money to {recipient['full_name']}? (y/n): ").lower()
-    if transfer_funds == 'n':
-        print("Transaction cancelled.  Have a nice day!")
-    else:
-        funds_available = False
-        transfer_amount = int(input(f"\nHow much money would you like to transfer to {recipient['full_name']}? (Whole dollars only): "))
-        while funds_available == False:
-            if transfer_amount > recipient['account_balance']:
-                print(f"Insufficient Funds.  Your transfer must be less than or equal to {payor['account_balance']}.  Please enter a smaller amount to transfer.")
-                transfer_amount = int(input(f"How much money would you like to transfer to {recipient['account_balance']}? (Whole dollars only): "))
-            else: 
-                funds_available = True
+# def transaction (payor,recipient):
+#     transfer_funds = input(f"Are you sure that you want to transfer money to {recipient['full_name']}? (y/n): ").lower()
+#     if transfer_funds == 'n':
+#         print("Transaction cancelled.  Have a nice day!")
+#     else:
+#         funds_available = False
+#         transfer_amount = int(input(f"\nHow much money would you like to transfer to {recipient['full_name']}? (Whole dollars only): "))
+#         while funds_available == False:
+#             if transfer_amount > payor['account_balance']:
+#                 print(f"Insufficient Funds.  Your transfer must be less than or equal to {payor['account_balance']}.  Please enter a smaller amount to transfer.")
+#                 transfer_amount = int(input(f"How much money would you like to transfer to {recipient['account_balance']}? (Whole dollars only): "))
+#             else: 
+#                 funds_available = True
 
-            print(f"\nInitiating transfer of {transfer_amount} to \"{recipient['full_name']}\".\n")
-            payor['account_balance']-=transfer_amount
-            recipient['account_balance']+=transfer_amount
-            print(f"Transaction successful!  You now have ${payor['account_balance']} remaining.")
+#             print(f"\nInitiating transfer of {transfer_amount} to \"{recipient['full_name']}\".\n")
+#             payor['account_balance']-=transfer_amount
+#             recipient['account_balance']+=transfer_amount
+#             print(f"Transaction successful!  You now have ${payor['account_balance']} remaining.")
         
-            additional_transaction=input("Would you like to make an additional transfer? (y/n): ").lower()
-            if additional_transaction == 'n':
-                print(f"\nYou now have {payor['account_balance']} left in your account.  Have a GREAT day!")
-                transfer_confirmed = False
-                
+#             additional_transaction=input("Would you like to make an additional transfer? (y/n): ").lower()
+#             if additional_transaction == 'n':
+#                 print(f"\nYou now have {payor['account_balance']} left in your account.  Have a GREAT day!")
+
 # Checking available balance function
 # def balance_check (payor, transfer_amount)
 # while
 # Calling Functions
-user_name_check (payor)
-password_check (payor)
-display_account_info (payor)
-transaction (payor,recipient)
+# transaction (payor,recipient)
